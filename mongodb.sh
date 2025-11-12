49a6e81388a4ce86070754128b25d22d78483c0a
Your Name <your.email@example.com>
2025-11-12 17:01:55 +0530

    practice

--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FOLDER="/var/log/shell--roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #means /var/log/shell-practice/16-logs.log
 
