5da5bcc00acaab56468a84071841d472d1a0ce4f
Your Name <your.email@example.com>
2025-11-12 16:47:03 +0530

    practice

---
+++ mongodb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #means /var/log/shell-practice/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "script start executed at: $(date)" | tee -a $LOG_FILE  # echo printed one to APPEND in log file
+
+
+if [ $USERID -ne 0 ]; then
+  echo -e " $R ERROR  $N: please run this script with root previlege"
+  exit 1
+fi
+
+VALIDATE(){     
+    if  [ $1 -ne 0 ]; then
+      echo -e " $2 IS  $R FAILURE $N" | tee -a $LOG_FILE
+      exit 1
+   else
+      echo -e " $2 is $G success $N" | tee -a $LOG_FILE
+   fi
+}
