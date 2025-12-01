8a7502e08e10cc7896167b9ce89cd6cb7038fbf4
Your Name <your.email@example.com>
2025-12-01 17:12:40 +0530

    practice

---
+++ redis.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #means /var/log/shell-roboshop/mongodb.log
+START_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+echo "script start executed at: $(date)" | tee -a $LOG_FILE  # echo printed one to APPEND in log file
+
+
+if [ $USERID -ne 0 ]; then
+  echo -e " $R ERROR  $N: please run this script with root previlege"
+  exit 1
+fi
+
+VALIDATE(){     
+    if  [ $1 -ne 0 ]; then
+      echo -e " $2 IS  $R FAILURE $N" | tee -a $LOG_FILE
+      exit 1
+   else
+      echo -e " $2 is $G success $N" | tee -a $LOG_FILE
+   fi
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default Redis"
+
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Redis 7"
+
+dnf install redis -y  &>>$LOG_FILE
+VALIDATE $? "Installing Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf 
+#2 changes so 2 times -e    protect mode changes (c) and to edit no there space and give no 
+#permanent change so -i
+VALIDATE $? "Allowing Remote connections to Redis"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
+
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
