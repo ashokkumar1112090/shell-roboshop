2848fa49c0a1c3e999b639dc64baa200bdc293de
Your Name <your.email@example.com>
2025-12-01 18:58:25 +0530

    practice

--- shipping.sh
+++ shipping.sh
@@ -10,7 +10,7 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MYSQL_HOST=mysql.daws86s.fun
+MYSQL_HOST=mysql.ashokking.sbs
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
