4c2bccb81d152bc74c38d2027293f284fcb69cec
Your Name <your.email@example.com>
2025-12-01 09:45:59 +0530

    practice

--- mongodb.sh
+++ mongodb.sh
@@ -36,3 +36,9 @@ VALIDATE $? "enable mongodb"
 
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Start mongodb" 
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongo.confg
+VALIDATE $? "allowing remote connection to mongodb"
+
+systemctl restart mongodb
+VALIDATE $? "restarted mongo db"
